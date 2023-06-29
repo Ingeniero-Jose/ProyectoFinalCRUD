5a5a2cb69a7ef075a85ab9d6ea76c4931763a928
jose_lopez <joseykalila7@gmail.com>
2023-06-29 10:58:43 -0700

    Darle un poco de vista

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,7 @@ const crearTarea = (event) => {
      tarea,
      descripcion,
   };
+
   if (localStorage.getItem("todoList") === null){
      //Se crea un arreglo(ARRAY)
      let todoList = [];
@@ -18,14 +19,15 @@ const crearTarea = (event) => {
 
      //Guardar la lista en el localStorage
      localStorage.setItem("todoList", JSON.stringify(todoList));
-  }else {
+  } else {
      let todoList = JSON.parse(localStorage.getItem("todoList"));
 
      //Añadir el nuevo elemento a la lista
      todoList.push(tareasTODO);
 
-     //Volveremos a guardar la lista en el localStorage
-  localStorage.setItem("todolist", JSON.stringify(todoList));
+   //Volveremos a guardar la lista en el localStorage
+ localStorage.setItem("todolist", JSON.stringify(todoList));
+
   }
 
   //Aqui vamos a limpiar nuestro formulario
@@ -52,41 +54,9 @@ const renderTodoList = () => {
      <button class ="button is-warning is-mall">Actualizar</button>
      <button class ="button is-danger is-mall">Eliminar</button>
      </td>
-     
      </tr>
      `;
      
   });
 };
 renderTodoList();
-
-const deleteTask = (title) => {
-  /* Vamos a traer la lista de elementos desde nuestro navegador */
-  let todoList = JSON.parse(localStorage.getItem("todoList"));
-  todoList = todoList.filter((task) => task.title !== title);
-  localStorage.setItem("todoList", JSON.stringify(todoList));
-  renderTodoList();
-};
-
-const updateTask = (title) => {
-  /* Vamos a traer la lista de elementos desde nuestro navegador */
-  let todoList = JSON.parse(localStorage.getItem("todoList"));
-
-  //Buscar la tarea
-  for (let i = 0; i < todoList.length; i++) {
-    if (todoList[i].title === title) {
-      /* Actualizar la tarea con los nuevos valores */
-      todoList[i].title = document.getElementById("title").value;
-      todoList[i].description = document.getElementById("description").value;
-      break;
-    }
-  }
-
-  /* Campos del formulario que queden vacios */
-  document.getElementById("title").value = "";
-  document.getElementById("description").value = "";
-
-  //Actualizamos nuestro lS con la lista actualizada
-  localStorage.setItem("todoList", JSON.stringify(todoList));
-  renderTodoList();
-}
